dd1f1e651a04e1e5cc60812269f39cc3c1d6f738
Mihai Mogosanu <mike@sapiensworks.com>
2015-09-22 17:35:35 -0400

    Added static Yes/No values for YesNoResult class

--- src/CavemanTools/Infrastructure/YesNoResult.cs
+++ src/CavemanTools/Infrastructure/YesNoResult.cs
@@ -2,6 +2,10 @@
 {
     public class YesNoResult
     {
+        public static YesNoResult Yes=new YesNoResult(true);
+        public static YesNoResult No=new YesNoResult(false);
+
+
         public bool Value { get; private set; }
 
         public YesNoResult(bool value)
