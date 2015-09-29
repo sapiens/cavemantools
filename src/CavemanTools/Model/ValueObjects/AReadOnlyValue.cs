e943c2a1c9897467c105a7b99ca555e84dbe934c
Mihai Mogosanu <mike@sapiensworks.com>
2015-09-29 18:47:28 -0400

    added AReadOnlyValue base class

---
+++ src/CavemanTools/Model/ValueObjects/AReadOnlyValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CavemanTools.Model.ValueObjects
+{
+    public abstract class AReadOnlyValue<T> : Tuple<T>
+    {
+        protected AReadOnlyValue(T item1) : base(item1)
+        {
+        }
+
+        public T Value => Item1;
+    }
+}
