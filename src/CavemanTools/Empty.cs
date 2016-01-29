d3587979ed3458d895310376911d1b8bf27ee4f8
Mihai Mogosanu <mike@sapiensworks.com>
2016-01-28 22:46:09 -0500

    ported to C# 6

--- src/CavemanTools/Empty.cs
+++ src/CavemanTools/Empty.cs
@@ -6,29 +6,14 @@ namespace CavemanTools
     {
         public static readonly Action Action = () => { };
 
-        public static Action<T> ActionOf<T>()
-        {
-            return t => { };
-        }
+        public static Action<T> ActionOf<T>() => t => { };
 
-        public static Func<T> Func<T>()
-        {
-            return () => default(T);
-        }
+        public static Func<T> Func<T>() => () => default(T);
 
-        public static Func<T, T> FuncOf<T>()
-        {
-            return t => t;  
-        }
+        public static Func<T, T> FuncOf<T>() => t => t;
 
-        public static Action GetOrEmpty(this Action act)
-        {
-            return act ?? Action;
-        }
+        public static Action GetOrEmpty(this Action act) => act ?? Action;
 
-        public static Action<T> GetOrEmpty<T>(this Action<T> act)
-        {
-            return act ?? ActionOf<T>();
-        }
+        public static Action<T> GetOrEmpty<T>(this Action<T> act) => act ?? ActionOf<T>();
     }
 }
