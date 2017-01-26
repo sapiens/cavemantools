3e998c3f6a713d2e519aa3c93da573433fb1a7f0
Mihai Mogosanu <mike@sapiensworks.com>
2017-01-26 14:06:55 -0500

    refactored MultilingualTexts and added new method

--- src/CavemanTools/Localization/MultilingualTexts.cs
+++ src/CavemanTools/Localization/MultilingualTexts.cs
@@ -6,22 +6,38 @@ namespace CavemanTools.Localization
 {
     public class MultilingualTexts
     {
-        List<Tuple<string,string,string>> _items=new List<Tuple<string, string, string>>();
+        class Item
+        {
+            public string Language;
+            public string Key;
+            public string Value;
+
+            public Item(string language, string key, string value)
+            {
+                Language = language;
+                Key = key;
+                Value = value;
+            }
+        }
+        List<Item> _items=new List<Item>();
         List<Translations> _translations=new List<Translations>();
         public void Add(Translations translations)
         {
             _translations.Add(translations);
-            _items.AddRange(translations.Select(kv => new Tuple<string, string, string>(translations.Language, kv.Key, kv.Value)));
+            _items.AddRange(translations.Select(kv => new Item(translations.Language, kv.Key, kv.Value)));
         }
 
         public IEnumerable<KeyValuePair<string, string>> TranslationFor(string textId)
         {
-            return _items.Where(t => t.Item2 == textId).Select(t => new KeyValuePair<string, string>(t.Item1, t.Item3));
+            return _items.Where(t => t.Key== textId).Select(t => new KeyValuePair<string, string>(t.Key, t.Value));
         }
 
         public Translations GetTranslations(string language)
         {
             return _translations.Find(t => t.Language == language);
         }
+
+        public IEnumerable<string> GetLanguageForValue(string value)
+            => _items.Where(d => d.Value == value).Select(d=>d.Language);
     }
 }
