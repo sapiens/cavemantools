7bde5e16104729578c1b532f7884425bbbfd055d
Mihai Mogosanu <mike@sapiensworks.com>
2017-03-12 18:09:41 -0400

    added async interface for unique store

--- src/CavemanTools/Model/Persistence/UniqueStore/IStoreUniqueValuesAsync.cs
+++ src/CavemanTools/Model/Persistence/UniqueStore/IStoreUniqueValuesAsync.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CavemanTools.Model.Persistence.UniqueStore
 {
-    public interface IStoreUniqueValues
+    public interface IStoreUniqueValuesAsync
     {
         /// <summary>
         /// 
@@ -10,25 +11,25 @@ namespace CavemanTools.Model.Persistence.UniqueStore
         /// <exception cref="UniqueStoreDuplicateException"></exception>
         /// <param name="item"></param>
         /// <returns></returns>
-        void Add(UniqueStoreItem item);
+        Task AddAsync(UniqueStoreItem item);
         /// <summary>
         /// Deletes all unique values associated with the entity
         /// </summary>
         /// <param name="entityId"></param>
-        void Delete(Guid entityId);
+        Task DeleteAsync(Guid entityId);
        
-        void Delete(string bucketId);
+        Task DeleteAsync(string bucketId);
         /// <summary>
         /// Delete values associated with the entity and an aspect
         /// </summary>
         /// <param name="item"></param>
-        void Delete(UniqueStoreDeleteItem item);
+        Task DeleteAsync(UniqueStoreDeleteItem item);
         /// <summary>
         /// Updates an unique value of an aspect for an entity
         /// </summary>
         /// <param name="item"></param>
         /// <exception cref="UniqueStoreDuplicateException"></exception>
         /// <returns></returns>
-        void Update(UniqueStoreUpdateItem item);
+        Task UpdateAsync(UniqueStoreUpdateItem item);
     }
 }
