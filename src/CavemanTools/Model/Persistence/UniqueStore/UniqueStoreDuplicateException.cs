60242379ccaeb8859379eae533526c3561162c1b
Mihai Mogosanu <mike@sapiensworks.com>
2017-03-12 18:09:51 -0400

    cleanup

---
+++ src/CavemanTools/Model/Persistence/UniqueStore/UniqueStoreDuplicateException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CavemanTools.Model.Persistence.UniqueStore
+{
+    public class UniqueStoreDuplicateException : Exception
+    {
+        public UniqueStoreDuplicateException()
+        {
+        }
+
+        public UniqueStoreDuplicateException(string message) : base(message)
+        {
+        }
+    }
+}
