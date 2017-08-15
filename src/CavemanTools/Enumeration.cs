39f234f006fed6017218335341b79d2e8186c211
Mihai Mogosanu <mike@sapiensworks.com>
2017-08-15 16:57:01 -0400

    other

--- src/CavemanTools/Enumeration.cs
+++ src/CavemanTools/Enumeration.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 
 namespace CavemanTools
 {
+    //public class Enumeration
+    //{
+    //    public Enumeration(int id, string displayName)
+    //    {
+    //        Id = id;
+    //        DisplayName = displayName;
+    //    }
+    //    /// <summary>
+    //    /// Field name containing a specific value of the enumeration. e.g: for Currency.USD field name is "USD"
+    //    /// </summary>
+    //    internal string Value { get; set; }
 
+    //    public int Id { get; private set; }
+    //    public string DisplayName { get; private set; }
 
+    //    public static IEnumerable<T> GetAll<T>() where T : Enumeration
+    //    {
+    //        return typeof(T).GetFields(BindingFlags.Static | BindingFlags.Public)
+    //            .Where(d => d.FieldType.DerivesFrom<Enumeration>())
+    //            .Select(d => d.GetValue(null)).Cast<T>();
+    //    }
+    //}
 }
