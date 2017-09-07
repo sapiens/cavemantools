3c9aa91df9cf3fe1aed8eb4b0cc7a052fafc0e7c
Mihai Mogosanu <mike@sapiensworks.com>
2017-09-07 13:09:35 -0400

    tests for Optional<T>

---
+++ src/Tests/Extensions/OptionalOfT_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CavemanTools;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.Extensions
+{
+    public class OptionalOfT_Tests
+    {
+        [Fact]
+        public void implicit_from_value()
+        {
+            Optional<PasswordHash> hash=new PasswordHash("test");
+            hash.HasValue.Should().BeTrue();
+        }
+        
+        [Fact]
+        public void implicit_to_value()
+        {
+            PasswordHash hash=new Optional<PasswordHash>(new PasswordHash("test"));
+            hash.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void empty_optional_implicit_to_value_throws()
+        {
+            Optional<PasswordHash> hash = Optional<PasswordHash>.Empty;
+            Assert.Throws<InvalidOperationException>(()=>
+            {
+                PasswordHash h = hash;
+            });            
+        }
+    }
+}
