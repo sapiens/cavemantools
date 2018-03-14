3fbbf272e0b67d2ddb66c891827714935b5787c5
Mihai Mogosanu <mike@sapiensworks.com>
2018-03-13 22:54:53 -0400

    added OperationId value object

---
+++ src/CavemanTools/Model/OperationId.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CavemanTools.Model
+{
+    public struct OperationId:IEquatable<OperationId>
+    {
+        public Guid Value { get; }
+
+        public OperationId(Guid value)
+        {
+            value.MustNotBeDefault();
+            Value = value;
+        }
+
+        public bool Equals(OperationId other)
+        {
+            return Value.Equals(other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is OperationId && Equals((OperationId) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
