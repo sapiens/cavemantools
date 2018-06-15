f2a568d73f3d5bdda4da8cbcf3976b76bae674a3
Mihai Mogosanu <mike@sapiensworks.com>
2018-06-15 09:57:57 -0400

    cleanup

--- src/CavemanTools/Infrastructure/ActivatorContainer.cs
+++ src/CavemanTools/Infrastructure/ActivatorContainer.cs
@@ -1,7 +1,5 @@
 using System;
-#if COREFX
 using System.Reflection;
-#endif
 
 namespace CavemanTools.Infrastructure
 {
@@ -13,12 +11,10 @@ namespace CavemanTools.Infrastructure
         private ActivatorContainer() : base(t =>
             {
 
-#if COREFX
+
                 var ti = t.GetTypeInfo();
                 if (ti.IsInterface || ti.IsAbstract) return null;
-#else
-                if (t.IsInterface || t.IsAbstract) return null;
-#endif
+
                 var res = Activator.CreateInstance(t);
                 return res;
             })
