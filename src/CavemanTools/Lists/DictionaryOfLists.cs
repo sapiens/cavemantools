37f6204f91c9d786e464c8b591ef79eae20b377a
Mihai Mogosanu <mmogosanu@webility.ca>
2022-03-12 14:17:50 -0500

    added dicitonary of lists class

---
+++ src/CavemanTools/Lists/DictionaryOfLists.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Collections.Generic
+{
+	public class DictionaryOfLists<TKey,TValue>:Dictionary<TKey,List<TValue>>
+	{
+		public DictionaryOfLists()
+		{
+			
+		}
+
+		public void AddValue(TKey key,TValue value)
+		{
+			var items = this.GetValueOrCreate(key, () => new List<TValue>());
+			items.Add(value);
+		}
+
+		public void AddMany(TKey key,IEnumerable<TValue> values)
+		{
+			var items = this.GetValueOrCreate(key, () => new List<TValue>());
+			items.AddRange(values);
+		}
+	}
+}
